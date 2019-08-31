3d90c68a76ff1d7886a420626c3c969f161c66ad
cristina1084 <cristina.johns1084@gmail.com>
2019-08-31 13:53:08 +0530

    first commit, created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddAlbumComponent } from './add-album/add-album.component';
+import { AddPhotoComponent } from './add-photo/add-photo.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAlbumComponent,
+    AddPhotoComponent,
+    GalleryComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
